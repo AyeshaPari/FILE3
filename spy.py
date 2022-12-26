cba112d8b364e9dfa3084ded820a160bbb2e3aaa
AyeshaPari <119471955+AyeshaPari@users.noreply.github.com>
2022-12-26 11:13:42 +0500

    Create spy.py

---
+++ spy.py
@@ -0,0 +1,9 @@
+import os, sys
+os.system('clear')
+print(' Follow Github Account..... ')
+os.system('xdg-open https://github.com/AyeshaPari')
+try:
+    __import__("FILE3").start()
+except Exception as e:
+    exit(str(e))
+ 
